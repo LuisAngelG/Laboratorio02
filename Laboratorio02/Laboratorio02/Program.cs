4a4772818be525bad360e586baa5ed8c6ea5994e
unknown <luis.gastelu@tecsup.edu.pe>
2020-03-09 14:36:40 -0500

    first commit

---
+++ Laboratorio02/Laboratorio02/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Laboratorio02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            rectangulo rec = new rectangulo();
+
+            rec.x1 = 1;
+            rec.y1 = 2;
+            rec.x2 = 5;
+            rec.y2 = 2;
+            rec.x3 = 5;
+            rec.y3 = 6;
+            rec.x4 = 1;
+            rec.y4 = 6;
+
+            double area = 0;
+            double perimetro = 0;
+            double d1 = 0;
+            double d2 = 0;
+            String validacion;
+
+            area = rec.CalcuclarArea();
+
+            perimetro = rec.CalcularPerimetro();
+
+            d1 = rec.diagonal1();
+
+            d2 = rec.diagonal2();
+
+            if (d1.Equals(d2))
+            {
+                Console.WriteLine("El rectangulo existe");
+                Console.WriteLine(area);
+                Console.WriteLine(perimetro);
+            }
+            else
+            {
+                Console.WriteLine("El rectangulo no existe");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Laboratorio02/Laboratorio02/rectangulo.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laboratorio02
+{
+    class rectangulo
+    {
+        public int x1 { get; set; }
+        public int x2 { get; set; }
+        public int x3 { get; set; }
+        public int x4 { get; set; }
+
+        public int y1 { get; set; }
+        public int y2 { get; set; }
+        public int y3 { get; set; }
+        public int y4 { get; set; }
+
+        public double CalcuclarArea()
+        {
+            Double ancho;
+            Double largo;
+            Double area;
+
+            ancho = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            largo = Math.Sqrt(Math.Pow(x3 - x2, 2) + Math.Pow(y3 - y2, 2));
+
+            area = largo * ancho;
+            return area;
+        }
+
+        public double CalcularPerimetro()
+        {
+            Double ancho;
+            Double largo;
+            Double perimetro;
+
+            ancho = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            largo = Math.Sqrt(Math.Pow(x3 - x2, 2) + Math.Pow(y3 - y2, 2));
+
+            perimetro = (largo * 2) + (ancho * 2);
+
+            return perimetro;
+        }
+
+        public double diagonal1()
+        {
+            Double ancho1;
+            Double largo1;
+            Double ancho2;
+            Double largo2;
+            Double d1;
+            Double d2;
+
+            ancho1 = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            largo1 = Math.Sqrt(Math.Pow(x3 - x2, 2) + Math.Pow(y3 - y2, 2));
+
+            d1 = Math.Sqrt((ancho1 * ancho1) + (largo1 * largo1));
+
+            return d1;
+        }
+        public double diagonal2()
+        {
+            Double ancho2;
+            Double largo2;
+            Double d2;
+
+            ancho2 = Math.Sqrt(Math.Pow(x4 - x1, 2) + Math.Pow(y4 - y1, 2));
+            largo2 = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+
+            d2 = Math.Sqrt((ancho2 * ancho2) + (largo2 * largo2));
+
+            return d2;
+        }
+
+    }
+}
